5da5c6234468fa7ce93dc1d91ce5bd626778f3e8
Don Livanec <donlivanec@gmail.com>
2012-11-01 09:32:25 -0400

    Insertering borough links

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,9 @@ Borough.destroy_all
 Location.destroy_all
 
 manhattan = Borough.create(name:"manhattan")
+bronx = Borough.create(name:"bronx")
+queens = Borough.create(name:"queens")
+brooklyn = Borough.create(name:"brooklyn")
+staten = Borough.create(name:"staten_island")
+
 Location.create(address:"45 Rockefeller Plz", borough:manhattan)
