fe5c6067412db07abe0b3cf22d96e45e043737f1
Don Livanec <donlivanec@gmail.com>
2012-11-01 11:25:26 -0400

    Making listing dynamic

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,5 @@
 module LocationsHelper
+  def is_safe location
+    location.safe ? "safe" : "notsafe"
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ActiveRecord::Base
-  attr_accessible :address, :borough_id, :latitude, :longitude, :verified, :borough
+  attr_accessible :address, :borough_id, :latitude, :longitude, :verified, :borough, :safe
   belongs_to :borough
+
 end
