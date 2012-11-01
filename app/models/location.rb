37d2a2924073d2673d075f01d6649a6b30b89953
Shea Gonyo <shea@illcreative.com>
2012-11-01 15:22:50 -0400

    styled edit form

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  attr_accessible :address, :borough_id, :latitude, :longitude, :verified, :borough, :safe, :elevators_safe, :elevators_available
+  attr_accessible :address, :borough_id, :latitude, :longitude, :verified, :borough, :safe, :power, :elevators_safe, :elevators_available
   belongs_to :borough
 
 end
